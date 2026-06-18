9b5129e0890dd6f3d9a54cc2c0c0b79dcf117ba6
Luisa3012 <l.grimm@mci4me.at>
2026-06-18 10:03:01 +0200

    Commit Test

---
+++ data/test.py
@@ -0,0 +1,9 @@
+
+
+class Test:
+    def __init__(self, test_id, person_id, date, test_type, ekg_data):
+        self.test_id = test_id
+        self.person_id = person_id
+        self.date = date
+        self.test_type = test_type
+        self.ekg_data = ekg_data
---
+++ person.py
@@ -0,0 +1,53 @@
+import json
+from PIL import Image
+
+
+class Person:
+
+    def __init__(self, id : int, date_of_birth : int, firstname, lastname, picture_path, ekg_tests, gender = "Male"):
+        self.id = id
+        self.date_of_birth = date_of_birth
+        self.firstname = firstname
+        self.lastname = lastname
+        self.picture_path = picture_path
+        self.ekg_tests = ekg_tests
+        self.age = self.calc_age()
+        self.gender = gender
+        self.max_hr = self.calc_max_heart_rate()
+
+
+    def set_hr(self, hr):
+        self.hr_max = hr
+
+    def get_full_name(self):
+        return self.lastname + ", " + self.firstname
+
+
+    def get_image(self):
+        image = Image.open(self.picture_path)
+        return image
+    
+    def load_by_id (self, id):
+        persons = get_person_data()
+        for person in persons:
+            if person.id == id:
+                return person
+        return None
+    
+    def calc_age(self): 
+        current_year = 2026
+        age = current_year-self.date_of_birth
+        return age
+    
+    def calc_max_heart_rate(self):
+        age = self.calc_age()
+        gender = self.gender
+        if gender == "Male":
+            return (208-(0.7*age))
+        elif gender == "Female":
+            return (206-(0.88*age))
+        else: 
+            return (220-age)
+    
+    def __str__(self):
+        return "{Person: self.get_full_name()}, Geb.{self.date_of_birth}, Alter:{sel.age}"
