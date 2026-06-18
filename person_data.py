885a97c4481af3fc72b2456581876b793d8b033f
Clara <kc3679@mci4me.at>
2026-06-18 11:41:10 +0200

    commit 2 person_data

--- person_data.py
+++ person_data.py
@@ -1,15 +1,17 @@
+import json
+import person
 
 
 class person_data: 
 
     def load_persons(self, filename):
-    with open(filename, "r") as file:
-        self.persons = json.load(file)
+        with open(filename, "r") as file:
+            self.persons = json.load(file)
 
-    return self.persons
+        return self.persons
 
     def load_by_id (self, id):
-        persons = get_person_data()
+        persons = self.load_persons("persons.json")
         for person in persons:
             if person.id == id:
                 return person
@@ -25,15 +27,15 @@ class person_data:
         else: 
             return (220-age)
         
-        
+
     def get_test(self, person_id, test_id):
-    person = self.get_person_by_id(person_id)
+        person = self.get_person_by_id(person_id)
 
-    if person is None:
+        if person is None:
+            return None
+
+        for test in person["ekg_tests"]:
+            if test["test_id"] == test_id:
+                return test
+
         return None
-
-    for test in person["ekg_tests"]:
-        if test["test_id"] == test_id:
-            return test
-
-    return None
